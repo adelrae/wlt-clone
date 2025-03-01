c7aaf21411d01258009f6e3f9c129f68c4985b6f
Adel Raisi <adel.udesign@gmail.com>
2025-03-02 02:16:53 +0330

    did some cleaning

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,35 +1,18 @@
 import * as THREE from "three";
 import { useLoader, useThree } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
 
 const Background = () => {
   const { camera } = useThree();
   const texture = useLoader(THREE.TextureLoader, "/sky_hdri7.webp");
-  const textures = useLoader(THREE.TextureLoader, []);
 
   texture.offset.y = -0.09; // Move the texture UP
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
 
   return (
     <>
-      <mesh position-z={-800} rotation={[-0.3, Math.PI, 0]}>
+      <mesh position-z={-800} position-y={-100}>
         <sphereGeometry args={[600, 8, 8]} />
         <meshBasicMaterial map={texture} side={THREE.BackSide} />
       </mesh>
-
-      {/* <Environment
-        resolution={32}
-        files={[
-          "/Standard-Cube-Map/px.png",
-          "/Standard-Cube-Map/nx.png",
-          "/Standard-Cube-Map/py.png",
-          "/Standard-Cube-Map/ny.png",
-          "/Standard-Cube-Map/pz.png",
-          "/Standard-Cube-Map/nz.png",
-        ]}
-        background
-      /> */}
     </>
   );
 };
