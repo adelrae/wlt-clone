0f81f5a513dd05a1f78dbb5424548747027b78af
Adel Raisi <adel.udesign@gmail.com>
2025-03-03 16:53:45 +0330

    fixed camera movement

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -6,9 +6,8 @@ import { CAMERA_START_POSITION, CAMERA_PATH } from "../utils/config";
 import { useCamera } from "../contexts/Camera";
 
 // Define constants
-const rotationThreshold = 1; // Start tilting and rotating at t=0.9
-const maxTiltAngle = -Math.PI / 4; // Negative 45 degrees for downward tilt
-const maxOrbitAngle = Math.PI; // 180 degrees orbit
+const ROTATION_THRESHOLD = 1; // Start tilting and rotating at t=0.9
+const MAX_TILT_ANGLE = -Math.PI / 4.3; // Negative 45 degrees for downward tilt
 
 const Camera = () => {
   const scroll = useScroll();
@@ -28,23 +27,21 @@ const Camera = () => {
     lerpedScrollOffset = Math.max(lerpedScrollOffset, 0);
     lastScroll.current = lerpedScrollOffset;
 
-    if (lerpedScrollOffset <= rotationThreshold) {
-      // **Phase 1: Follow the path downward**
-      const pathT = lerpedScrollOffset / rotationThreshold;
-      const curPoint = CAMERA_PATH.getPoint(Math.min(pathT, 1));
-      cameraGroup.current?.position.lerp(curPoint, delta * 20);
+    // Follow the path downward
+    const pathT = lerpedScrollOffset / ROTATION_THRESHOLD;
+    const curPoint = CAMERA_PATH.getPoint(Math.min(pathT, 1));
+    cameraGroup.current?.position.lerp(curPoint, delta * 20);
 
-      // Apply downward tilt
-      const tiltProgress = THREE.MathUtils.smoothstep(
-        lerpedScrollOffset,
-        0.7,
-        rotationThreshold
-      );
-      const tiltAngle = tiltProgress * maxTiltAngle;
-      cameraGroup.current.rotation.set(tiltAngle, 0, 0);
-    } else {
-    }
+    // Apply downward tilt
+    const tiltProgress = THREE.MathUtils.smoothstep(
+      lerpedScrollOffset,
+      0.7,
+      ROTATION_THRESHOLD
+    );
+    const tiltAngle = tiltProgress * MAX_TILT_ANGLE;
+    cameraGroup.current.rotation.set(tiltAngle, 0, 0);
 
+    // Update
     setCameraPosition(cameraGroup.current?.position);
     setLerpedScrollOffset(lerpedScrollOffset);
   });
